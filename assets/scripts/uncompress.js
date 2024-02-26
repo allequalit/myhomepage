271de15d531066515a728113b5f15eee97a8ed3a
allequalit <50491988+allequalit@users.noreply.github.com>
2024-02-27 03:24:59 +0900

    Add files via upload

---
+++ assets/scripts/uncompress.js
@@ -0,0 +1,59 @@
+function redirectToExpressVPN() {
+    // 사용자에게 알림을 보여줍니다.
+    alert("최고의 VPN 서비스 제공업체, ExpressVPN으로 이동합니다");
+    // 함수에서 true를 반환하면 기본 링크 동작을 계속 진행합니다.
+    // 여기서는 사용자가 확인을 누르면 지정된 href로 이동합니다.
+    return true;
+}
+
+function t1server() {
+    ChannelIO('openSupportBot', '36964'); 
+}
+function t2server() {
+    ChannelIO('openSupportBot', '36964'); 
+}
+function t3server() {
+    ChannelIO('openSupportBot', '36964'); 
+}
+function t4server() {
+    ChannelIO('openSupportBot', '95183'); 
+}
+
+function tm1server() {
+    ChannelIO('openSupportBot', '95184'); 
+}
+function tm2server() {
+    ChannelIO('openSupportBot', '95185'); 
+}
+function tm3server() {
+    ChannelIO('openSupportBot', '36964'); 
+}
+function tm4server() {
+    ChannelIO('openSupportBot', '95187'); 
+}
+
+function k1server() {
+    ChannelIO('openSupportBot', '95304'); 
+}
+function k2server() {
+    ChannelIO('openSupportBot', '95305'); 
+}
+function k3server() {
+    ChannelIO('openSupportBot', '36964'); 
+}
+function k4server() {
+    ChannelIO('openSupportBot', '95308'); 
+}
+
+function km1server() {
+    ChannelIO('openSupportBot', '95309'); 
+}
+function km2server() {
+    ChannelIO('openSupportBot', '36964'); 
+}
+function km3server() {
+    ChannelIO('openSupportBot', '95311'); 
+}
+function km4server() {
+    ChannelIO('openSupportBot', '36964'); 
+}
