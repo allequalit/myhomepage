e205bc0dc4acbb24037da578ee827df74738c2c1
allequalit <50491988+allequalit@users.noreply.github.com>
2024-03-15 14:21:08 +0900

    Add files via upload

---
+++ price/calculatePrice.js
@@ -0,0 +1,34 @@
+function calculatePrice() {
+    const country = document.getElementById('country').value;
+    const ports = parseInt(document.getElementById('ports').value, 10);
+    const publicIPs = parseInt(document.getElementById('publicIPs').value, 10);
+    const users = parseInt(document.getElementById('users').value, 10);
+
+    let basePrice = country === 'japan' ? 1000 : 100000;
+    let portPrice = country === 'japan' ? 0 : 3000 * ports;
+    let publicIPPrice = country === 'japan' ? 2000 * publicIPs : 50000 * publicIPs;
+    let userPrice = 0;
+
+    if (country === 'japan') {
+        if (users <= 10) userPrice = 1000 * users;
+        else if (users <= 50) userPrice = 800 * users;
+        else if (users <= 100) userPrice = 600 * users;
+        else if (users <= 200) userPrice = 400 * users;
+    } else if (country === 'korea') {
+        if (users <= 10) userPrice = 1500 * users;
+        else if (users <= 50) userPrice = 1200 * users;
+        else if (users <= 100) userPrice = 1000 * users;
+        else if (users <= 200) userPrice = 900 * users;
+    }
+
+    const totalPrice = basePrice + portPrice + publicIPPrice + userPrice;
+
+     // 숫자를 통화 형식으로 변환
+     const formatter = new Intl.NumberFormat('ko-KR', {
+        style: 'currency',
+        currency: 'KRW',
+    });
+    const formattedTotalPrice = formatter.format(totalPrice);
+
+    document.getElementById('totalPrice').innerText = formattedTotalPrice;
+}
